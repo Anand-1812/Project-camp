35b3f5e64a86ed11ad199de7f3bfe800f2f88f3b
Anand-1812 <anand.work2005@gmail.com>
2025-09-16 15:58:30 +0530

    env file

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: "./.env",
+});
+
+let myUserName = process.env.username;
+console.log("Name: ", myUserName);
+
 console.log("Starting....");
