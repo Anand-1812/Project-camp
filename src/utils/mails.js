331e64dc1c413c6e6e941c7cfe8e9858528bd5b8
Anand-1812 <anand.work2005@gmail.com>
2025-09-24 14:55:00 +0530

    feat: email verification

---
+++ src/utils/mails.js
@@ -0,0 +1,16 @@
+var email = {
+  body: {
+    name: "John Appleseed",
+    intro: "Welcome to Mailgen! We're very excited to have you on board.",
+    action: {
+      instructions: "To get started with Mailgen, please click here:",
+      button: {
+        color: "#22BC66",
+        text: "Confirm your account",
+        link: "https://mailgen.js/confirm?s=d9729feb74992cc3482b350163a1a010",
+      },
+    },
+    outro:
+      "Need help, or have questions? Just reply to this email, we'd love to help.",
+  },
+};
