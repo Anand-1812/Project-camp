8dc4986d8fa213ef57582b87859544db13593361
Anand-1812 <anand.work2005@gmail.com>
2025-09-28 19:52:22 +0530

    add: login routes

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import cors from "cors";
 import express from "express";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(cookieParser());
 
 // cors configuratio
 app.use(
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -5,7 +5,7 @@ import { asyncHandler } from "../utils/async_handler.js";
 import { sendEmail } from "../utils/mails.js";
 import { emailVerificationMailgenContent } from "../utils/mailgen_content.js"; // ensure this exists
 
-const generateAccessToken = async (userId) => {
+const generateAccessAndRefreshToken = async (userId) => {
   try {
     const user = await User.findById(userId);
     if (!user) {
@@ -83,6 +83,55 @@ export const registerUser = asyncHandler(async (req, res) => {
         200,
         { user: createdUser },
         "User registered successfully and verification email has been sent to your email",
+      ),
+    );
+});
+
+const login = asyncHandler(async (req, res) => {
+  const { email, password, username } = req.body;
+
+  if (!username || !email) {
+    throw new ApiError(400, "Username or email is required");
+  }
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new ApiError(400, "user does not exist");
+  }
+
+  const isPasswordValid = User.isPasswordCorrect(password);
+
+  if (!isPasswordValid) {
+    throw new ApiError(400, "Invalid credentials");
+  }
+
+  const { accessToken, refreshToken } = await generateAccessAndRefreshToken(
+    user._id,
+  );
+
+  const loggedInUser = await User.findById(user._id).select(
+    "-password -refreshToken -emailVerificaionToken -emailVerificaionExpiry",
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        {
+          user: loggedInUser,
+          accessToken,
+          refreshToken,
+        },
+        "User logged in successfully",
       ),
     );
 });
